c2640d6bdd57c42ecb8aa758e9cd404019c9439d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2016-11-10 16:45:26 +0100

    interceptors

--- app/01_interceptor/app.js
+++ app/01_interceptor/app.js
@@ -1,19 +1,21 @@
 (function() {
 	'use strict';
 
-	var app = angular.module('myApp', []);
+	var app = angular.module('myApp', ['jlg-spinner', 'jlg-delay']);
 
 	app.run(function($rootScope, $http, $q) {
 		'ngInject';
 		$rootScope.start = function() {
 			console.log('start', arguments);
-			$http.get('./ws/s1').then(function(response) {
-				return $q.all([$http.get('./ws/s2'), $http.get('./ws/s3'), $http.get('./ws/s4')]);
-			}).then(function(response) {
-				return $http.get('./ws/s5');
-			}).catch(function(error) {
-				console.error('error', error);
-			});
+			$http.get('./ws/s1')
+				.then(function(response) {
+					return $q.all([$http.get('./ws/s2'), $http.get('./ws/s3'), $http.get('./ws/s4')]);
+				}).then(function(response) {
+					return $http.get('./ws/s5');
+				}).catch(function(error) {
+					console.error('error', error);
+				});
 		};
 	});
+
 })();
