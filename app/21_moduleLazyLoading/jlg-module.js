234db0af3dca56a61775ab2fa7bb91219791a7b3
Jean-Louis GUENEGO <jlguenego@gmail.com>
2016-11-13 18:04:02 +0100

    lazy loading config

--- app/21_moduleLazyLoading/jlg-module.js
+++ app/21_moduleLazyLoading/jlg-module.js
@@ -3,8 +3,14 @@
 
 	var app = angular.module('myModule', []);
 
+	app.config(function() {
+		'ngInject';
+		console.log('myModule config');
+	});
+
 	app.run(function($rootScope) {
 		'ngInject';
+		console.log('myModule run');
 		$rootScope.message = 'coucou!';
 	});
 
