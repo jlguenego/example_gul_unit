fddd76fbd78a697b3daa07a21f9faeec0ad0ad30
Jean-Louis GUENEGO <jlguenego@gmail.com>
2016-11-20 13:09:15 +0100

    decorator + log

--- app/02_decorator/app.js
+++ app/02_decorator/app.js
@@ -1,14 +1,31 @@
 (function() {
 	'use strict';
 
-	var app = angular.module('myApp', ['jlg-log']);
+	var app = angular.module('myApp', []);
+
+	app.config(['$provide', function ($provide) {
+		$provide.decorator('$rootScope', function ($delegate) {
+			var emit = $delegate.$emit;
+
+			$delegate.$emit = function () {
+				console.log.apply(console, arguments);
+				emit.apply(this, arguments);
+			};
+
+			return $delegate;
+		});
+	}]);
 
 	app.run(function($rootScope, $http, $log) {
 		'ngInject';
 		$rootScope.start = function() {
-			$log.debug('start', arguments);
-			$log.debug('end', arguments);
+			// throw an event
+			$rootScope.$emit('coucou');
 		};
+
+		$rootScope.$on('coucou', function() {
+			console.log('hello !');
+		});
 	});
 
 })();
---
+++ app/18_log/app.js
@@ -0,0 +1,14 @@
+(function() {
+	'use strict';
+
+	var app = angular.module('myApp', ['jlg-log']);
+
+	app.run(function($rootScope, $http, $log) {
+		'ngInject';
+		$rootScope.start = function() {
+			$log.debug('start', arguments);
+			$log.debug('end', arguments);
+		};
+	});
+
+})();
