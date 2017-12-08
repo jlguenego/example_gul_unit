5982d3627bd9d357535b0622c13e9c77a4e426fa
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-12-08 08:52:11 +0100

    create modules

--- app/01_interceptor/app.js
+++ app/01_interceptor/app.js
@@ -16,28 +16,6 @@
 					console.error('error', error);
 				});
 		};
-
-		async function xhr(url) {
-			return $http.get(url).then(response => Promise.resolve(response)).catch(error => Promise.reject(error));
-		}
-
-		$rootScope.start2 = async () => {
-			console.log('start2');
-			try {
-				const s1 = await xhr('./ws/s1');
-				const s2 = xhr('./ws/s2');
-				const s3 = xhr('./ws/s3');
-				const s4 = xhr('./ws/s4');
-				await s2;
-				await s3;
-				await s4;
-				const s5 = await xhr('./ws/s5');
-			} catch (e) {
-				console.error('error', e);
-			}
-			console.log('end');
-			
-		};
 	});
 
 })();
--- app/29_asyncawait/app.js
+++ app/29_asyncawait/app.js
@@ -25,7 +25,6 @@
 				console.error('error', e);
 			}
 			console.log('end');
-			
 		};
 	});
 
