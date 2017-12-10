bf18edad58c3bf6a1864fc20cebb2219536d10c4
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-12-10 11:29:52 +0100

    quiz

---
+++ app/32_quiz/main.js
@@ -0,0 +1,8 @@
+import { questions } from './question.js';
+import '../modules/jlg-quiz/jlg-quiz.module';
+
+const app = angular.module('main', ['jlg-quiz']);
+
+app.run((jlgQuiz) => {
+	jlgQuiz.questions = questions;
+});
---
+++ app/modules/jlg-quiz/jlg-quiz.module.js
@@ -0,0 +1,71 @@
+import finishedHtml from './finished.html';
+import startHtml from './start.html';
+import questionHtml from './question.html';
+import footerHtml from './footer.html';
+
+const app = angular.module('jlg-quiz', []);
+
+app.service('jlgQuiz', function JLGQuiz() {
+	this.questions = undefined;
+});
+
+app.component('jlgQuiz', {
+	controller: function JLGQuizCtrl($scope, $element, $compile, jlgQuiz) {
+        this.jlgQuiz = jlgQuiz;
+        
+		const START = 0;
+		const QUESTION = 1;
+		const END = 2;
+		const templates = [startHtml, questionHtml, finishedHtml];
+
+		this.state = START;
+		this.template = templates[this.state];
+
+		this.i = 0;
+
+		this.next = () => {
+            console.log('next');
+			if (this.state === START) {
+                if (jlgQuiz.questions.length > 0) {
+                    this.state = QUESTION;
+                } else {
+                    this.state = END;
+                }
+			} else if (this.state === QUESTION) {
+				this.i++;
+				if (this.i === jlgQuiz.questions.length) {
+					this.state = END;
+				}
+			}
+			this.render();
+		};
+		this.prev = () => {
+            console.log('prev');
+			if (this.state === END) {
+                if (jlgQuiz.questions.length > 0) {
+                    this.i--;
+                    this.state = QUESTION;
+                } else {
+                    this.state = START;
+                }
+			} else if (this.state === QUESTION) {
+				if (this.i === 0) {
+					this.state = START;
+				} else {
+                    this.i--;
+                }
+			}
+			this.render();
+		};
+
+		this.render = () => {
+            this.template = templates[this.state];
+			
+
+			this.template += footerHtml;
+			$element.html(this.template);
+			$compile($element.contents())($scope);
+		};
+		this.render();
+	}
+});
